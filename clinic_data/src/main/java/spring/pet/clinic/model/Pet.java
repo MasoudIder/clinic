f987b09794a538a2c7ff272a24be321a0d6d3141
administrator <masoudider@yahoo.com>
2019-11-27 12:58:28 +0330

    Entity

--- clinic_data/src/main/java/spring/pet/clinic/model/Pet.java
+++ clinic_data/src/main/java/spring/pet/clinic/model/Pet.java
@@ -1,12 +1,24 @@
 package spring.pet.clinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_type_id")
     private PetType petType;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- clinic_data/src/main/java/spring/pet/clinic/model/Speciality.java
+++ clinic_data/src/main/java/spring/pet/clinic/model/Speciality.java
@@ -1,5 +1,11 @@
 package spring.pet.clinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Speciality extends BaseEntity {
 
     private String description;
